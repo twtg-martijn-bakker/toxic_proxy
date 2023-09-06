13cf7312396717a007d8ef60c304b71116489fe2
Martijn Bakker <martijn.bakker@twtg.io>
2023-09-06 15:20:25 +0200

    add entrypoint for poetry script

--- toxic_proxy/__init__.py
+++ toxic_proxy/__init__.py
@@ -1,1 +1,22 @@
-from .app import toxic_proxy
+import logging
+from .app import toxic_proxy, asyncio
+
+def run():
+    logging.basicConfig(level=logging.INFO)
+    p = toxic_proxy(
+        destination=('localhost', 1884),
+        port=1883
+    )
+
+    loop = asyncio.get_event_loop()
+    server = loop.run_until_complete(p)
+    logging.info('Listening on %s:%u', *server.sockets[0].getsockname())
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
